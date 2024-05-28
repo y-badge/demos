8781ce864cb589e6fd2170c741dced731c6f262c
Philip Lundrigan <lundrigan@byu.edu>
2024-05-28 10:03:50 -0600

    WIP: Trying to get global yboard object working

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
 #include "Arduino.h"
-#include "yboardv2.h"
-#include "yboardv3.h"
-
-#include "hardware_test/hardware_test.h"
-#include "light_show/light_show.h"
-#include "wifi_sniffer/wifi_sniffer.h"
-
-YBoardV2 yboard;
-// YBoardV3 yboard;
+#include "yboard.h"
 
 void setup() {
-    // wifi_sniffer_init(&yboard);
-    // hardware_test_init(&yboard);
-    light_show_init(&yboard);
+    Serial.begin(9600);
+    yboard.setup();
 }
 
-void loop() {
-    // wifi_sniffer_loop();
-    // hardware_test_loop();
-    light_show_loop();
-}
+void loop() {}
